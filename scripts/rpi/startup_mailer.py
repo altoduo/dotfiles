69f2b2fb07e4e68f5f8595dc99f48c8af94a6c82
Colin Heinzmann <cheinzmann3@gmail.com>
2013-09-06 02:37:36 -0400

    got startup-mailer.py to work!! yayy raspberry pis can now send a notification email on boot!

--- scripts/rpi/startup_mailer.py
+++ scripts/rpi/startup_mailer.py
@@ -1,32 +1,43 @@
+#!/usr/bin/python3
+
 import subprocess
 import smtplib
 import socket
 import os
 from email.mime.text import MIMEText
 import datetime
+
 # Change to your own account information
-to ='EMAIL ADDRESS HERE'
-gmail_user ='raspi3141592@gmail.com'
-gmail_password = 'PASSWORD HERE'
+to ='RECIPIENT'
+gmail_user ='SENDER EMAIL'
+gmail_password = 'SENDER EMAIL PASSWORD'
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
 smtpserver.ehlo()
 smtpserver.starttls()
-smtpserver.ehlo
+smtpserver.ehlo()
 smtpserver.login(gmail_user, gmail_password)
 today = datetime.date.today()
-# Very Linux Specific
+
+# Get the ip address of the main route on system
 arg='ip route list'
 p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
-data = p.communicate()
-split_data = data[0].split()
-ipaddr = split_data[split_data.index('src')+1]
-# Get the current CPU speed
-f = os.popen('/opt/vc/bin/vcgencmd get_config arm_freq')
-cpu = f.read()
-mail_body = "CPU speed: " + cpu + "IP address: %s" %  ipaddr
+data = str(p.communicate()[0])
+ipaddr = data.split(' ')[2]
+
+# Cpu speed doesn't work either, save for later
+cpu = 'INVALID'
+
+# get the hostname of rpi
+hostname = socket.gethostname()
+
+# craft the email
+#mail_body = "CPU speed: " + cpu + "IP address: %s" %  ipaddr
+mail_body = "IP address: " + ipaddr + "\n" + "CPU Speed: " + cpu
 msg = MIMEText(mail_body)
-msg['Subject'] = "RasPI @ "+ipaddr+" started up on %s" % today.strftime('%b %d %Y')
+msg['Subject'] = hostname + " @ " + ipaddr + " started up on %s" % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
 msg['To'] = to
+
+# and send the mail
 smtpserver.sendmail(gmail_user, [to], msg.as_string())
 smtpserver.quit()
